b305a3ce52c300dfdb9a4187697db500518f8279
Bram van Es <bramiozo@gmail.com>
2025-06-12 09:12:16 +0200

    beginning on patchtst

---
+++ scripts/pre_training_mlm.py
@@ -0,0 +1,10 @@
+# args
+# ------
+# model_name: any long context MLM
+# batch_size: 16
+# learning_rate: 1e-5
+# decay_rate: 1e-3
+# training_strategy: Literal['cyclic', 'linear']
+#
+# Tokenizer: from tokenizer.sax import ECGTokenizer
+#
