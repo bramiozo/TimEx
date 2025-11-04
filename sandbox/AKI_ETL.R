e3ec783199453279498628da142acf310dab1020
Bram van Es <bramiozo@gmail.com>
2025-11-04 16:03:52 +0100

    Auto stash before merge of "main" and "origin/main"

--- sandbox/AKI_ETL.R
+++ sandbox/AKI_ETL.R
@@ -14,9 +14,9 @@ library(data.table)
 library(arrow)
 library(sas7bdat)
 # 'L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\data_preparation_DV_final_for_LCMM_15257AKIpatients_FU7-3650d_SW365d_TR365d_13032025.Rdata'
-load("L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\data_preparation_DV_final_for_LCMM_15257AKIpatients_FU7-3650d_SW365d_TR365d_13032025.Rdata")
-write_parquet(dataset, "L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\data_preparation_DV_final_for_LCMM_15257AKIpatients_FU7-3650d_SW365d_TR365d_13032025.parquet")
+load("T:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\28102025\\ANALYSIS\\BRAM\\cleaned_DV_LCMM_data.Rdata")
+write_parquet(LCMM_data, "T:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\28102025\\ANALYSIS\\BRAM\\cleaned_DV_LCMM_data.parquet")
 
 
-load("L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\cleaned_DV_final.Rdata")
-write_parquet(data, "L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\cleaned_DV_final.parquet")
+#load("L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\cleaned_DV_final.Rdata")
+#write_parquet(data, "L:\\lab_research\\RES-Folder-UPOD\\NOSTRADAMUS_SALTRO\\E_ResearchData\\2_ResearchData\\CLEANED_for_Methods_paper_longitudinal_analysis\\13032025\\cleaned_DV_final.parquet")
