0efcf1fc3c031f89128e38b0592b9fc49ac7f394
Bram van Es <bramiozo@gmail.com>
2025-11-04 16:03:18 +0100

    making cross-sectional clusterer

--- src/timex/clustering.py
+++ src/timex/clustering.py
@@ -1,25 +1,35 @@
-# exposes clustering methods from tslearn, aeon, tscluster, deeptime,  and pypots
+# exposes clustering methods from tslearn, aeon, tscluster, deeptime, and pypots
 # [ ] tslearn
 # [ ] aeon
 # [ ] tscluster
 # [ ] deeptime
 # [ ] pypots
 
+from asyncio import SelectorEventLoop
+from timex import extractor
+from timex import preprocessing
+
+from pandas import DataFrame
+from numpy import ndarray
+
 from tslearn import clustering as tslearn_clustering
 
+from sklearn.cluster import GaussianMixture, BayesianGaussianMixture
+from sklearn.base import BaseEstimator, ClusterMixin
 
-class TSKMeans():
+class TSKMeans:
     # https://tslearn.readthedocs.io/en/stable/gen_modules/clustering/tslearn.clustering.TimeSeriesKMeans.html#tslearn.clustering.TimeSeriesKMeans
-    def __init__(self, backend='tslearn', n_clusters=3, **kwargs):
-        if backend=='tslearn':
-            self.clusterer = tslearn_clustering.TimeSeriesKMeans(n_clusters=n_clusters, **kwargs)
+    def __init__(self, backend="tslearn", n_clusters=3, **kwargs):
+        if backend == "tslearn":
+            self.clusterer = tslearn_clustering.TimeSeriesKMeans(
+                n_clusters=n_clusters, **kwargs
+            )
 
-    
     def fit(self, ts):
         pass
 
 
-class TSKernelKMeans():
+class TSKernelKMeans:
     # https://tslearn.readthedocs.io/en/stable/gen_modules/clustering/tslearn.clustering.KernelKMeans.html#tslearn.clustering.KernelKMeans
     def __init__(self):
         pass
@@ -27,10 +37,116 @@ class TSKernelKMeans():
     def fit(self, ts):
         pass
 
-class TSKShape():
+
+class TSKShape:
     # https://tslearn.readthedocs.io/en/stable/gen_modules/clustering/tslearn.clustering.KShape.html#tslearn.clustering.KShape
     def __init__(self):
         pass
 
     def fit(self, ts):
         pass
+
+
+class CrossSectionalClustering(BaseEstimator, ClusterMixin):
+    # https://tslearn.readthedocs.io/en/stable/gen_modules/clustering/tslearn.clustering.CrossSectionalClustering.html#tslearn.clustering.CrossSectionalClustering
+    def __init__(
+        self,
+        smoothing: bool=False,
+        smoothing_type: Literal["gaussian_kernel", "rolling_mean"],
+        smoothing_window_size: int = 10,
+        interpolation: bool = False,
+        interpolation_resolution: int = 30,
+        interpolation_keep_init: bool = False,
+        min_measurements_per_id: int = 10,
+        n_clusters: int = 3,
+        random_state: int = None,
+        extractors: List[
+            Literal[
+                "custom",
+                "tsfresh",
+                "catch22",
+                "cesium",
+                "antropy",
+                "nolds",
+                "katz",
+                "tsfel",
+            ]
+        ] = None,
+        clustering_algorithm: Literal["gmm", "bgmm"],
+        normalise_timeseries: Optional[Literal["bulk", "group"]] = None,
+        normalisation_method: Optional[Literal["standard", "minmax"]] = None,
+        id_column: str,
+        time_column: str,
+        feature_columns: str,
+    ):
+        """
+        Initialize the CrossSectionalClustering class.
+
+        We assume that we have multiple time series per id.
+        We assume that the time_column is numeric and represents the unit of time.
+        We assume that the feature_columns are numeric and represent the features of the time series
+
+        Args:
+            smoothing (bool, optional): Whether to apply smoothing to the time series. Defaults to False.
+            smoothing_type (Literal["gaussian_kernel", "rolling_mean"], optional): The type of smoothing to apply. Defaults to "gaussian_kernel".
+            smoothing_window_size (int, optional): The window size for smoothing. Defaults to 10.
+            interpolation (bool, optional): Whether to interpolate the time series. Defaults to False.
+            interpolation_resolution (int, optional): The resolution for interpolation. Defaults to 30.
+            min_measurements_per_id (int, optional): The minimum number of measurements per time series. Defaults to 10.
+            n_clusters (int, optional): The number of clusters to form. Defaults to 3.
+            random_state (int, optional): The random state for reproducibility. Defaults to None.
+            extractors (List[Literal["custom", "tsfresh", "catch22", "cesium", "antropy", "nolds", "katz", "tsfel"]], optional): The feature extractors to use. Defaults to None.
+            normalise_timeseries (bool, optional): Whether to normalise the time series. Defaults to False.
+            normalisation_method (Literal["zscore", "minmax", "robust"], optional): The method for normalising the time series. Defaults to "zscore".
+            id_column (str): The column to use for identifying time series.
+            time_column (str): The column to use for time information.
+            feature_columns (List[str]): The columns to use for feature extraction.
+        """
+
+        self.smoothing = smoothing
+        self.smoothing_type = smoothing_type
+        self.smoothing_window_size = smoothing_window_size
+        self.interpolation = interpolation
+        self.interpolation_resolution = interpolation_resolution
+        self.interpolation_keep_init = interpolation_keep_init
+        self.min_measurements_per_id = min_measurements_per_id
+        self.n_clusters = n_clusters
+        self.random_state = random_state
+        self.extractors = extractors
+        self.normalise_timeseries = normalise_timeseries
+        self.normalisation_method = normalisation_method
+        self.id_column = id_column
+        self.time_column = time_column
+        self.feature_columns = feature_columns
+
+        if clustering_algorithm == "gmm":
+            self.clustering_algorithm = GaussianMixture(n_components=n_clusters, random_state=random_state)
+        elif clustering_algorithm == "bgmm":
+            self.clustering_algorithm = BayesianGaussianMixture(n_components=n_clusters, random_state=random_state)
+        else:
+            raise ValueError("Invalid clustering algorithm")
+
+    def fit(self, ts: DataFrame, y = None):
+        id_kwargs = {"id_col": self.id_column, "time_col": self.time_column, "val_col": self.feature_columns}
+
+        if self.interpolation:
+            ts = preprocessing.get_interpolated(ts, time_res=self.interpolation_resolution, keep_t0_value=self.interpolation_keep_init, df_out=True, **id_kwargs)
+
+        if self.smoothing:
+            if self.smoothing_type == 'gaussian_kernel':
+                ts = preprocessing.get_smoothed_gaussian_kernel(ts, window=self.smoothing_window_size, Nskip=3, df_out=True, **id_kwargs)
+            elif self.smoothing_type == 'savgol':
+                ts = preprocessing.get_smoothed_savgol(ts, window=self.smoothing_window_size, polyorder=self.smoothing_polyorder, df_out=True, **id_kwargs)
+            elif self.smoothing_type == 'rolling_mean':
+                ts = preprocessing.get_smoothed_rolling_mean(ts, window=self.smoothing_window_size, df_out=True, **id_kwargs)
+            else:
+                raise ValueError("Invalid smoothing type")
+
+        if self.normalise_timeseries:
+            ts = preprocessing.normalise_ts(ts, scaler=self.normalisation_method, df_out=True, id_kwargs**)
+
+
+        self.clustering_algorithm.fit(ts)
+
+    def predict(self, ts: DataFrame):
+        return clustering_algorithm.predict(ts)
--- src/timex/preprocessing.py
+++ src/timex/preprocessing.py
@@ -5,16 +5,16 @@
 from scipy import signal
 from scipy import interpolate
 from scipy import ndimage
-#from scipy.stats import skew, kurtosis
-#from scipy.fft import rfft, rfftfreq
-#from scipy.signal import cwt, ricker
+# from scipy.stats import skew, kurtosis
+# from scipy.fft import rfft, rfftfreq
+# from scipy.signal import cwt, ricker
 
 import numpy as np
 import tsfresh
 
 from tqdm import tqdm
 import sys, os
-import pandas as pd 
+import pandas as pd
 import gc
 from collections import defaultdict
 from time import sleep
@@ -25,28 +25,32 @@ from typing import Optional, List, Dict, Literal
 
 from tslearn import metrics
 
-def normalise_ts(ts_df, id_col='ID', 
-                 time_col='Time_days', 
-                 val_col='eGFR_CKDEpi2012',
-                 df_out=False,
-                 scaler: Literal['standard', 'minmax'] = 'standard'):
-    if scaler=='standard':
+
+def normalise_ts(
+    ts_df,
+    id_col="ID",
+    time_col="Time_days",
+    val_col="eGFR_CKDEpi2012",
+    df_out=False,
+    scaler: Literal["standard", "minmax"] = "standard",
+):
+    if scaler == "standard":
         scaler = TimeSeriesScalerMeanVariance()
-    elif scaler=='minmax':
+    elif scaler == "minmax":
         scaler = TimeSeriesScalerMinMax()
-    
-    #group_scaler = GroupedTransformer(scaler, groups=id_col)
-    #ts_df[val_col] = group_scaler.fit_transform(ts_df)    
-    
+
+    # group_scaler = GroupedTransformer(scaler, groups=id_col)
+    # ts_df[val_col] = group_scaler.fit_transform(ts_df)
+
     res = {id_col: [], time_col: [], val_col: []}
     for _id in tqdm(ts_df[id_col].unique()):
-        _df = ts_df.loc[ts_df[id_col]==_id][[id_col, time_col, val_col]]
+        _df = ts_df.loc[ts_df[id_col] == _id][[id_col, time_col, val_col]]
         ts = _df[val_col].values
         ts = ts.reshape(1, -1, 1)
         ts = scaler.fit_transform(ts)
         ts = ts.reshape(-1)
         _df[val_col] = ts
-        
+
         res[id_col].extend(_df[id_col].values)
         res[time_col].extend(_df[time_col].values)
         res[val_col].extend(_df[val_col].values)
@@ -55,29 +59,36 @@ def normalise_ts(ts_df, id_col='ID',
     else:
         return res
 
-def get_filtered_df(ts_df, 
-                    id_col='ID',
-                    time_col='Time_days', 
-                    min_days=365, 
-                    min_measurements=3):
+
+def get_filtered_df(
+    ts_df, id_col="ID", time_col="Time_days", min_days=365, min_measurements=3
+):
     maxts = ts_df.groupby(id_col)[time_col].max()
-    ltids = maxts[maxts>min_days].index
-    
-    cnts = ts_df[(ts_df[id_col].isin(ltids)) &
-                 (ts_df[time_col]<min_days)].groupby(id_col).size()
-    filtered_ids = cnts[cnts>min_measurements].index
-    
+    ltids = maxts[maxts > min_days].index
+
+    cnts = (
+        ts_df[(ts_df[id_col].isin(ltids)) & (ts_df[time_col] < min_days)]
+        .groupby(id_col)
+        .size()
+    )
+    filtered_ids = cnts[cnts > min_measurements].index
+
     ts = ts_df.loc[ts_df[id_col].isin(filtered_ids)]
     return ts
 
-def get_interpolated(ts_df, id_col='ID', time_col='Time_days',
-                     val_col='eGFR_CKDEpi2012',
-                     days_before=0,
-                     max_days=365,
-                     time_res=7,
-                     keep_t0_value=False,
-                     df_out=False):
-    '''
+
+def get_interpolated(
+    ts_df,
+    id_col="ID",
+    time_col="Time_days",
+    val_col="eGFR_CKDEpi2012",
+    days_before=0,
+    max_days=365,
+    time_res=7,
+    keep_t0_value=False,
+    df_out=False,
+):
+    """
     Extract interpolated time series data.
 
     :param ts_df: pd.DataFrame, time series data
@@ -89,143 +100,174 @@ def get_interpolated(ts_df, id_col='ID', time_col='Time_days',
     :param time_res: int, time resolution of time series
     :param keep_t0_value: boolean, whether to keep t0 value
     :param df_out: boolean, whether to output dataframe
-    '''
+    """
     trange = np.arange(-days_before, max_days, time_res)
-    if (keep_t0_value) & (days_before>0):
+    if (keep_t0_value) & (days_before > 0):
         trange = np.insert(trange, 1, 0)
 
     res = {id_col: [], time_col: [], val_col: []}
     for _id in tqdm(ts_df[id_col].unique()):
-        _df = ts_df.loc[ts_df[id_col]==_id][[time_col, val_col]]
+        _df = ts_df.loc[ts_df[id_col] == _id][[time_col, val_col]]
         x = _df[time_col]
         y = _df[val_col]
         interpolator = interpolate.PchipInterpolator(x, y, extrapolate=False)
         interpolated = interpolator(trange)
-        
-        ids = len(trange)*[_id]
+
+        ids = len(trange) * [_id]
 
         res[id_col].extend(ids)
         res[time_col].extend(trange)
         res[val_col].extend(interpolated)
-    
+
     if df_out:
-        return pd.DataFrame.from_dict(res, orient='columns')
+        return pd.DataFrame.from_dict(res, orient="columns")
     else:
         return res
 
+
 # TODO: ensure that first N-points are excluded from smoothing
-def get_smoothed_rolling_mean(ts_dict: dict, 
-                              id_col='ID', 
-                              time_col='Time_days',
-                              val_col='eGFR_CKDEpi2012', 
-                              window=5,
-                              Nskip=3,
-                              df_out=False):
+def get_smoothed_rolling_mean(
+    ts_df: pd.DataFrame,
+    id_col: str = "ID",
+    time_col: str = "Time_days",
+    val_col: str = "eGFR_CKDEpi2012",
+    window: int = 5,
+    Nskip: int = 3,
+    df_out: bool = False,
+):
     res = {id_col: [], time_col: [], val_col: []}
-    
-    ts_df = pd.DataFrame.from_dict(ts_dict)
+
     for _id in tqdm(ts_df[id_col].unique()):
-        _df = ts_df.loc[ts_df[id_col]==_id][[id_col, time_col, val_col]]
+        _df = ts_df.loc[ts_df[id_col] == _id][[id_col, time_col, val_col]]
         _df = _df.sort_values(by=time_col)
         if Nskip > 0:
-            r = range(Nskip, _df.shape[0]-Nskip)
+            r = range(Nskip, _df.shape[0] - Nskip)
         else:
             r = range(_df.shape[0])
-        _df.iloc[r, 2] = _df[val_col].rolling(window=window,
-                                              min_periods=0).mean().values[r]
-        
+        _df.iloc[r, 2] = (
+            _df[val_col].rolling(window=window, min_periods=0).mean().values[r]
+        )
+
         res[id_col].extend(_df[id_col].values)
         res[time_col].extend(_df[time_col].values)
         res[val_col].extend(_df[val_col].values)
     if df_out:
-        return pd.DataFrame.from_dict(res, orient='columns')
+        return pd.DataFrame.from_dict(res, orient="columns")
     else:
         return res
 
+
 # TODO: ensure that first N-points are excluded from smoothing
-def get_smoothed_gaussian_kernel(ts_dict: dict, 
-                                 id_col='ID', 
-                                 time_col='Time_days',
-                                 val_col='eGFR_CKDEpi2012', 
-                                 window=5,
-                                 Nskip=3,
-                                 df_out=False):
-    res = {
-            id_col: [], 
-            time_col: [], 
-            val_col: []
-        }
-    
-    ts_df = pd.DataFrame.from_dict(ts_dict)
+def get_smoothed_gaussian_kernel(
+    ts_df: pd.DataFrame,
+    id_col: str = "ID",
+    time_col: str = "Time_days",
+    val_col: str = "eGFR_CKDEpi2012",
+    window: int = 5,
+    Nskip: int = 3,
+    df_out: bool = False,
+):
+    res = {id_col: [], time_col: [], val_col: []}
+
     for _id in tqdm(ts_df[id_col].unique()):
-        _df = ts_df.loc[ts_df[id_col]==_id][[id_col, time_col, val_col]]
+        _df = ts_df.loc[ts_df[id_col] == _id][[id_col, time_col, val_col]]
         _df = _df.sort_values(by=time_col)
         smoothed_values = ndimage.gaussian_filter1d(_df[val_col], window)
 
         if Nskip > 0:
-            r = range(Nskip, len(_df)-Nskip)
+            r = range(Nskip, len(_df) - Nskip)
         else:
             r = range(len(_df))
 
         _df.iloc[r, 2] = smoothed_values[r]
-        
+
         res[id_col].extend(_df[id_col].values)
         res[time_col].extend(_df[time_col].values)
         res[val_col].extend(_df[val_col].values)
     if df_out:
-        return pd.DataFrame.from_dict(res, orient='columns')
+        return pd.DataFrame.from_dict(res, orient="columns")
     else:
         return res
+
+
+# function get_smoothed_savgol(ts, window=self.smoothing_window_size, polyorder=self.smoothing_polyorder, df_out=True, **id_kwargs)
+def get_smoothed_savgol(
+    ts_df: pd.DataFrame,
+    id_col: str = "ID",
+    time_col: str = "Time_days",
+    val_col: str = "eGFR_CKDEpi2012",
+    window: int = self.smoothing_window_size,
+    polyorder: int = self.smoothing_polyorder,
+    Nskip: int = 3,
+    df_out: bool = True,
+):
+    smoothed_values = signal.savgol_filter(ts, window, polyorder)
+
+    if Nskip > 0:
+        r = range(Nskip, len(_df) - Nskip)
+    else:
+        r = range(len(_df))
+
+    if df_out:
+        return pd.DataFrame(smoothed_values, columns=["smoothed"])
+    else:
+        return smoothed_values
 
 
 # perform low pass filtering
-def get_low_pass_filtered(ts_dict: dict, id_col='ID', time_col='Time_days',
-                        val_col='eGFR_CKDEpi2012', cutoff=0.25, stationary=True,
-                        order=1, btype='low', window=3, df_out=False):
+def get_low_pass_filtered(
+    ts_dict: dict,
+    id_col="ID",
+    time_col="Time_days",
+    val_col="eGFR_CKDEpi2012",
+    cutoff=0.25,
+    stationary=True,
+    order=1,
+    btype="low",
+    window=3,
+    df_out=False,
+):
     res = {id_col: [], time_col: [], val_col: []}
-    '''
-    Should normaly only be applied for uniformly sampled time series that 
+    """
+    Should normaly only be applied for uniformly sampled time series that
     are stationary and have a constant sampling rate.
     If stationary is set to False
-    '''
-    
+    """
+
     if stationary:
         for _id in tqdm(ts_dict[id_col].unique()):
-            _df = ts_dict.loc[ts_dict[id_col]==_id][[id_col, time_col, val_col]]
+            _df = ts_dict.loc[ts_dict[id_col] == _id][[id_col, time_col, val_col]]
             _df = _df.sort_values(by=time_col)
             vals = _df[val_col].values
-            filtered = signal.butter(order, cutoff, 
-                                         btype='low', 
-                                         analog=False, 
-                                         output='sos')
+            filtered = signal.butter(
+                order, cutoff, btype="low", analog=False, output="sos"
+            )
             filtered = signal.sosfilt(filtered, vals)
-            
+
             res[id_col].extend(_df[id_col].values)
             res[time_col].extend(_df[time_col].values)
             res[val_col].extend(filtered)
-    else:       
+    else:
         for _id in tqdm(ts_dict[id_col].unique()):
-            _df = ts_dict.loc[ts_dict[id_col]==_id][[id_col, time_col, val_col]]
+            _df = ts_dict.loc[ts_dict[id_col] == _id][[id_col, time_col, val_col]]
             _df = _df.sort_values(by=time_col)
-            
+
             vals = _df[val_col].values
-            trend = ndimage.gaussian_filter1d(vals, window)   
+            trend = ndimage.gaussian_filter1d(vals, window)
             trend[-window:] = vals[-window:]
-            
-            detrended = vals - trend        
-            passfilter = signal.butter(order, cutoff, 
-                                         btype='low', 
-                                         analog=False, 
-                                         output='sos')
-            filtered = signal.sosfilt(passfilter, detrended)             
+
+            detrended = vals - trend
+            passfilter = signal.butter(
+                order, cutoff, btype="low", analog=False, output="sos"
+            )
+            filtered = signal.sosfilt(passfilter, detrended)
             retrended = filtered + trend
-            
+
             res[id_col].extend(_df[id_col].values)
             res[time_col].extend(_df[time_col].values)
-            res[val_col].extend(retrended)       
-    
+            res[val_col].extend(retrended)
+
     if df_out:
-        return pd.DataFrame.from_dict(res, orient='columns')
+        return pd.DataFrame.from_dict(res, orient="columns")
     else:
         return res
-    
